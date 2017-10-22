41c47a44b918ba762dc0f7619a7c64a4e6e195d8
Jere-Joonas Valtanen <jerevaltanen@gmail.com>
2017-10-23 01:52:26 +0300

    Added comments

--- app/views/camera-view/camera-view.js
+++ app/views/camera-view/camera-view.js
@@ -1,52 +1,65 @@
 var page;
-var textSource;
-var textTarget;
+// kuva tallennetaan tähän muuttujaan
 var pic;
-var savedPic;
+// nappi jolla otetaan kuva
 var takePicButton;
+// tämä nappi vaihdetaan takepicbuttonin tilalle kun ensimmäinen kuva on otettu
 var takeAnotherPicButton;
 var imgurUploadButton;
-var imageSourceModule = require("image-source");
+// otetaan käyttöön kamera plugin, jolla päästään käyttämään kameraa
 var camera = require("nativescript-camera");
+// haetaan käyttöön social share plugin, auttaa jakamisessa
 var socialShare = require("nativescript-social-share");
+// kuva tallennetaan tähän muuttujaan
 var imageModule = require("ui/image");
 var frameModule = require("ui/frame");
+// kameran funktioon annetaan arguementtina options
 var options = {saveToGallery: true };
+// Haetaan käyttöön toast -plugin
 var Toast = require("nativescript-toast");
 
-//var clientId = "96e762d0ccd74f8";
-//var clientSecret = "0d57ac4d5af10e4e09905d7667b415b9298f00e4";
-
-
+// Kutsutaan sivun latautuessa
 exports.loaded = function(args) {
+  // page muuttujan kautta päästään käsiksi ui elementteihin
   page = args.object;
 
+  // haetaan share nappi
   imgurUploadButton = page.getViewById("imgurUploadButton");
+  // piilotetaan share nappi
   imgurUploadButton.visibility = "collapse";
+  // haetaan kuvan ui-image elementti valmiiksi
   pic = page.getViewById("picResult");
+  // kuvanottonappi haetaan
   takePicButton = page.getViewById("takePicButton");
   takeAnotherPicButton = page.getViewById("takePicButton2");
     // Pyydetään kameralle käyttöluvat
     camera.requestPermissions();
 
+    // näytetään uuden kuvan ottamisnappi jos kuvanottamisnappi on piilotettu
   if(takePicButton.visibility == "collapse")
   {
     imgurUploadButton.visibility = "visible";
   }
 };
 
+// kutsutaan kun painetaan nappia jolla otetaan kuva
 exports.takePicture = function(){
   // Otetaan kuva
   camera.takePicture(options)   
   // Tämä kutsutaan kun kuva on otettu
   .then(function (imageAsset) {
-      console.log("Result is an image asset instance");
+      //image muuttujaan tallennetaan kuva
       var image = new imageModule.Image();
+      // tallennetaan kuva muihin muuttujiin
       image.src = imageAsset;
       pic.src = imageAsset;
+      //piilotetaan nappi
       takePicButton.visibility = "collapsed";
+      //näytetään toinen nappi
       takeAnotherPicButton.visibility = "visible";
+      //näytetään share nappi
       imgurUploadButton.visibility = "visible";
+      // näytetään toast joka kertoo kuvan tallentuneen galleriaan
       var toast = Toast.makeText("Picture saved to gallery");
       toast.show();
   }).catch(function (err) {
@@ -54,12 +67,14 @@ exports.takePicture = function(){
   });
 };
 
+// Tätä kutsutaan kun painetaa share nappia
 exports.socialUpload = function(){
+  // social share plugin hoitaa jakamisen, annetaan sille vain kuva joka jaetaan ja jakoviesti
   socialShare.shareImage(pic.imageSource, "How would you like to share this image?");
 };
 
 exports.onNavBtnTap = onNavBtnTap;
-
+// kutsutaan kun painetaan takaisin nappia navi palkista
 function onNavBtnTap()
 {
   var frameModule = require("ui/frame");
--- platforms/android/src/main/assets/app/views/imgur-view/imgur-view.js
+++ platforms/android/src/main/assets/app/views/imgur-view/imgur-view.js
@@ -1,65 +1,84 @@
 var page;
 var Observable = require("data/observable").Observable;
 var ObservableArray = require("data/observable-array").ObservableArray;
+// Openurl plugin auttaa avamaan linkin selaimessa
 var OpenUrl = require( "nativescript-openurl" );
+// Haetaan käyttöön toast -plugin
 var Toast = require("nativescript-toast");
-var imageStackLayout;
-var obj;
+// http auttaa tekemään get kyselyn APIin
 var http = require("http");
-var items;
+// muuttuja joko imgurin logo tallennetaan
 var imgurLogo;
+// imgurin apin avain
 var clientId = "96e762d0ccd74f8";
-var clientSecret = "0d57ac4d5af10e4e09905d7667b415b9298f00e4";
+//var clientSecret = "0d57ac4d5af10e4e09905d7667b415b9298f00e4";
 var imageListView;
+// ObservableArray jota käytetään listan täyttöön
 var items = new ObservableArray([]);
+// Observable joka täyttää listan ui elementissä
 var pageData = new Observable();
+// apin kyselyn osoite
 var requestUrl = "https://api.imgur.com/3/gallery/hot/viral/0.json"
 
-
+// kutsutaan sivun latauteass
 exports.loaded = function(args) {
+  // page muuttujan kautta päästään käsiksi ui elementteihin
   page = args.object;
+  // pageData elementillä pystytään muuttamaan listview dataa
   page.bindingContext = pageData;
-  imgurImage = page.getViewById("imgurImage");
-  imageListView = page.getViewById("imageListView");
+  // Haetaan imgur logon ui elementti ja ladataan logo
   imgurLogo = page.getViewById("imgurLogo");
-  items = [];
   imgurLogo.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Imgur_logo.svg/2000px-Imgur_logo.svg.png";
   imgurLogo.width = 90;
+
+  //Haetaan listview elementti johon kuvat tuodaan
+  imageListView = page.getViewById("imageListView");
+  // observableArray johon näytettävä data tallennetaan
+  items = [];
 
   loadImages();
 };
 
+// Tässä funktiossa haetaan kuvat ja niiden tiedot sekä asetetaan ne listview elementtiin
 function loadImages()
 {
+  // http kysely
   http.request({ 
     url: requestUrl,
     method: "GET",
     headers: {"authorization": "Client-ID " + clientId}
   }).then(function (response) {
-    //// Argument (response) is HttpResponse!
-    //// Content property of the response is HttpContent!
+    // vastaus tulee response objektina
+    // response objektin content saadaan JSON objektiksi
     var jsonObj = response.content.toJSON();
 
+    // Haetaan 20 ensimmäistä kuvaa for loopilla
     for(i = 0; i < 20; i++) {
-
+      // element muuttujaan tallennetaan json objektin data
       var element = jsonObj.data[i];
       
+      // Jos kyseessä on yksittäinen kuva element.images on null
       if(element.images == null)
       {
       imgSrc = element.link.toString();
       }
+      // Jos kyseessä on albumi haetaan images arraystä albumin ensimmäinen kuva
       else
       {
         var data = element.images[0];
         imgSrc = data.link.toString();
       }
 
+      // lisätään äskettäin haetut asiat items listaan
+      // Tämä lista on siis suoraan yhteydessä ui-elementteihin {{imageSrc}} jne...
       items.push({
         imageSrc: imgSrc,
         imageDesc: element.title,
+        // itemId on linkki kuvaan
         itemId: element.id
       })
     }
+    // Tässä asetetaan items data ui-listaan
     pageData.set("items", items)
   }, function (e) {
     //// Argument (e) is Error!
@@ -67,7 +86,7 @@ function loadImages()
 }
 
 exports.onNavBtnTap = onNavBtnTap;
-
+// kutsutaan kun painetaan takaisin nappia navi palkista
 function onNavBtnTap()
 {
   var frameModule = require("ui/frame");
@@ -76,20 +95,27 @@ function onNavBtnTap()
   topmost.goBack();
 }
 
+// Kutsutaan kun jotakin listan kuvaa painetaan
 function onItemTap(args) {
   var itemIndex = args.index;
 
+  // tässä haetaan painetun kohdan ui elementit
   page = args.view;
+  // linkki kuvaan saadaan hakemalla piilotetun listImageId:n teksti
   link = page.getViewById("listImageId");
 
+  // käytetään openurl pluginia linkin avaamiseen, yksinkertaista
   OpenUrl("https://imgur.com/gallery/" + link.text);
 }
 exports.onItemTap = onItemTap;
 
+// kutsutaan kun painetaan navibarin refresh nappia
 exports.refresh = function(){
+  // nollataan listat ja kutsutaan loadImages uudelleen
   items = [];
   imageListView.refresh();
   loadImages();
+  // näytetään toast että lista on päivitetty
   var toast = Toast.makeText("List refreshed");
   toast.show();
 }
