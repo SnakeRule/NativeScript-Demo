14f7fdc6d66c8c4f81fba844538c0c5e7da33bab
bhnamn <borhan.amini@gmail.com>
2017-10-24 00:27:01 +0300

    google map corrected by new API Key

--- app/views/map-view/map-view.js
+++ app/views/map-view/map-view.js
@@ -1,4 +1,5 @@
 var page;
+var mapsModule = require("nativescript-google-maps-sdk");
 
 
 // kutsutaan sivun latauteass
@@ -17,3 +18,31 @@ function onNavBtnTap()
 
   topmost.goBack();
 }
+
+function onMapReady(args) {
+  var mapView = args.object;
+
+  console.log("Setting a marker...");
+  var marker = new mapsModule.Marker();
+  marker.position = mapsModule.Position.positionFromLatLng(62.2333333, 25.7333333);
+  marker.title = "Jyväskylä";
+  marker.snippet = "Suomi";
+  marker.userData = { index : 1};
+  mapView.addMarker(marker);
+  
+  // Disabling zoom gestures
+  mapView.settings.zoomGesturesEnabled = true;
+  mapView.settings.compassEnabled = true;
+}
+
+function onMarkerSelect(args) {
+   console.log("Clicked on " +args.marker.title);
+}
+
+function onCameraChanged(args) {
+    console.log("Camera changed: " + JSON.stringify(args.camera)); 
+}
+
+exports.onMapReady = onMapReady;
+exports.onMarkerSelect = onMarkerSelect;
+exports.onCameraChanged = onCameraChanged;
--- platforms/android/build/intermediates/assets/F0F1F2F3/debug/app/views/map-view/map-view.js
+++ platforms/android/build/intermediates/assets/F0F1F2F3/debug/app/views/map-view/map-view.js
@@ -1,12 +1,5 @@
-var page;
 
-
-// kutsutaan sivun latauteass
-exports.loaded = function(args) {
-  // page muuttujan kautta päästään käsiksi ui elementteihin
-  page = args.object;
-  
-};
+var mapsModule = require("nativescript-google-maps-sdk");
 
 exports.onNavBtnTap = onNavBtnTap;
 // kutsutaan kun painetaan takaisin nappia navi palkista
@@ -17,3 +10,31 @@ function onNavBtnTap()
 
   topmost.goBack();
 }
+
+function onMapReady(args) {
+  var mapView = args.object;
+
+  console.log("Setting a marker...");
+  var marker = new mapsModule.Marker();
+  marker.position = mapsModule.Position.positionFromLatLng(62.2333333, 25.7333333);
+  marker.title = "Sydney";
+  marker.snippet = "Australia";
+  marker.userData = { index : 1};
+  mapView.addMarker(marker);
+  
+  // Disabling zoom gestures
+  mapView.settings.zoomGesturesEnabled = false;
+  mapView.settings.compassEnabled = true;
+}
+
+function onMarkerSelect(args) {
+   console.log("Clicked on " +args.marker.title);
+}
+
+function onCameraChanged(args) {
+    console.log("Camera changed: " + JSON.stringify(args.camera)); 
+}
+
+exports.onMapReady = onMapReady;
+exports.onMarkerSelect = onMarkerSelect;
+exports.onCameraChanged = onCameraChanged;
